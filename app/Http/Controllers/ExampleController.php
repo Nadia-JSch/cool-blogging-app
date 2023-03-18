4d87e07fa494525b8a9e1ca644d77d38249fecfc
Nadia <Nadia>
2023-03-18 18:28:59 +0200

    Create the layout component and the home and about route views

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -10,15 +10,19 @@ class ExampleController extends Controller
 {
     // create a function
     public function homepage() {
-    // Step 4 - imagine we loaded data from the database
+    // Step 4 - imagine we loaded this data from a dynamic database query
         $ourName = 'Nadia';
+        // simple array
         $animals = ['Meowsalot', 'Barksalot', 'Purrsloud'];
 
+        // first argument is the name of the view we want to use
         // pass in data from DB into the view using an array with keys from an associate array
+        // => used to assign a value to the variable name 'allAnimals'
+        // we can have many pieces of data seperated by a comma 
         return view('homepage', ['allAnimals' => $animals, 'name' => $ourName, 'catName' => 'Meowsalot']);
     }
 
     public function aboutPage() {
-        return '<h1>About Page!!!</h1><a href="/">Back to home</a>';
+        return view('single-post');
     }
 }
